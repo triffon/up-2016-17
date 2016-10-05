3d16b6b82265df67c560bf71f198cdfc628d0aff
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-10-05 11:59:10 +0300

    Първа програма на C++

---
+++ hello/hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 5.10.2016 г.
+ *      Author: trifon
+ */
+
+#include <iostream>
+using namespace std;
+int main () {
+  int a = 5;
+  cout << "a = " << a << endl ;
+  cout << "2a = " << 2 * a << endl ;
+  return 0;
+}
