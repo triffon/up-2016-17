33dd5304c15f1cf762a5530986d14af7eac81733
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-10-21 00:41:33 +0300

    Примери за преобразуване на типове

--- vars/vars.cpp
+++ vars/vars.cpp
@@ -6,7 +6,9 @@
  */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 int hello () {
   const int N = 5;
   cout << N;
@@ -39,7 +41,7 @@ int bools() {
 	return 0;
 }
 
-int main() {
+int ints() {
 	long egn = 9512058274;
 	cout << egn << endl;
 	unsigned x = 5;
@@ -52,11 +54,71 @@ int main() {
 	// !!! (cout << a) < (b << endl);
 	cout << (a < b) << endl;
 	cout << ((a > b) > x) << endl;
+}
+
+int floats() {
 	// !!! float y = 1e-100;
-	double y = 1e-100;
+	double y = 1e-200;
+	cout << y << endl;
 	y = y * y;
 	cout << y << endl;
-	cout << (y == 0) << endl;
+	cout << (y == 0) << endl << endl;
+
+	const float EPS = 1E-3;
+
+	float z = 1.2;
+	float t = z * 0.3;
+	t = t * 0.3;
+	t = t / 0.9;
+	cout << (z == t) << endl;
+	cout << (fabs(z-t) < EPS) << endl << endl;
+//	z = z - 0.3;
+	z -= 0.3;
+	z = z - 0.6;
+	z = z - 0.3;
+	cout << z << endl;
+	cout << (z == 0) << endl;
+	cout << (fabs(z) < EPS) << endl;
+}
+
+int main() {
+	short s = 100;
+	long egn = 9876543210;
+	(((cout) << ((s) + (egn))) << endl);
+	2+3;
+	bool b = true;
+	char c = 'A';
+	cout << (char)(b + c) << endl;
+	double d = s + 0.3;// pow(s,100);
+	cout << d << endl;
+	short s2 = (short)d;
+	cout << s2 << endl;
+	// !!! cout << s / 3 << endl;
+	cout << s / 3.0 << endl;
+
+	char digit;
+	cout << "Въведете цифра: ";
+	cin >> digit;
+
+	int digitNumber = digit - '0';
+	cout << "Удвоената цифра: " << digitNumber * 2 << endl;
+	// !!! cout << digitNumber + 1 < 10 << endl;
+	// !!! (cout << (digitNumber + 1)) < (10 << endl);
+	cout << (digitNumber + 1 < 10) << endl;
+	cout << ((-s)-5) << endl;
+	{}{};;;;{}{{}{}{}{}}
+	s = 1.3;
+	cout << s << endl;
+;;;;;
+	cout << ++s << endl; // Връща s, lvalue
+	cout << s++ << endl; // връща старата стойност на s, rvalue
+	cout << s << endl;
+	{}{}{}{}{}
+	(++s) += 5;
+	cout << s << endl;
+
+	// !!! (s++) += 5
+	// !!! s++ + ++s;
 }
 
 
