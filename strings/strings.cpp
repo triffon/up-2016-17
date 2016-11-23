5eacc2a09e0d69a3a93b0ba5c24975d6bc8d95c6
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-11-23 09:57:51 +0200

    Проверка дали даден низ е палиндром.

---
+++ strings/strings.cpp
@@ -0,0 +1,62 @@
+/*
+ * strings.cpp
+ *
+ *  Created on: 23.11.2016 г.
+ *      Author: trifon
+ */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX = 100;
+
+int testStings() {
+	char q[6] = "Hello";
+	char s[MAX] = "Test"; // { '\0' };
+	char p[MAX] = "Hello";
+	cout << "s = ";
+	// !!! cin >> s;
+	cin.getline(s, MAX);
+	cout << s << ' ' << (void*)s << endl;
+	cout << p << ' ' << (void*)p << endl;
+	cout << q << ' ' << (void*)q << endl;
+	cout << strlen(s) << endl;
+	char s2[MAX];
+	strcpy(s2, s); // "s2 = s"
+	cout << s2 << endl;
+	cout << strcmp(s, s2) << endl;
+	cout << strcmp(s, p) << endl;
+	cout << (char)strcmp(s, p) << endl;
+	if (strcmp(s, p) > 0) cout << s << " е след " << p << endl;
+	strcat(s, " Goodbye, world!");
+	cout << s << ' ' << s2 << endl;
+	char c;
+	cout << "c = ";cin >> c;
+	if (strchr(s, c))
+		cout << "Среща се: " << strchr(s, c) << endl;
+	else
+		cout << "Не се среща!\n";
+	if (strstr(s, "or"))
+		cout << "Среща се: " << strstr(s, "or") << endl;
+	else
+		cout << "Не се среща!\n";
+
+	return 0;
+}
+
+
+int main() {
+	char s[MAX] = "";
+	cin.getline(s, MAX);
+	int n = strlen(s), mid = n / 2 - 1;
+	int i = 0;
+	while (i <= mid && s[i] == s[n-1-i])
+		i++;
+	// НЕ: s[i] != s[n-1-i]
+	// ДА: i > mid
+	cout << "Низът \"" << s << '"';
+	if (i <= mid)
+		cout << " НЕ";
+	cout << " е палиндром.\n";
+}
