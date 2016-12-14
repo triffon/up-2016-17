442d6033c69a6c7f0a4446e42cbc115c08e8a062
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-12-14 10:13:53 +0200

    Примери за работа с указатели, решение на задачата от малкото контолно

--- pointref/pointref.cpp
+++ pointref/pointref.cpp
@@ -10,12 +10,14 @@
 #include <iostream>
 using namespace std;
 
- void print(char* p) {
-    while (*p)
+ void print(char const* p) {
+    while (*p) {
+        // !!! if (*p == 'l') *p = 'X';
         cout << *p++;
+    }
 }
 
-int main() {
+int testPointers() {
 	double d = 1.23, *qd = &d, **qqd = &qd;
 	cout << d << ' ' << qd << ' ' << qqd << endl;
 	cout << *qd << ' ' << *qqd << endl;
@@ -41,9 +43,46 @@ int main() {
 
     char s[] = "Hello, world!";
     print(s);
+    print("Goodbye, world!");
     // !!! char* ps = "Hi C++!";
     // !!! ps[1] = 'o';
 	return 0;
+}
+
+int main() {
+    int a[10], *p = &a[2],
+               *pa[2] = { a + 3, &a[6] },
+               **q = &pa[1];
+    // !!! *p = &[2];
+//     p = a[2];
+    p = &a[2];
+    p = a + 2;
+    // p = *(a + 2); <-> p = a[2];
+    // !!!*p = &a[2];
+    // !!! *p = a[2];
+    pa[0] = &a[3];
+    pa[1] = pa[0] + 3;
+    // !!! *pa[0] = &a[3];
+    q = pa + 1;
+    // **q = ....
+    *(p + 2) = 8;
+    // !!! p + 2 = 8;
+    // !!! &(p + 2) = 8;
+    p[2] = 8;
+    // !!! **(q-2) = 8;
+    // !!! **(q-1+1) = 8; <-> **q = 8;
+    *(*q-2) = 8; // <-> pa[1]
+    *(*(q-1)+1) = 8;
+    *(*(q+0)-2) = 8; // <-> pa[1]
+    q[0][-2] = 8;
+    q[-1][1] = 8;
+
+
+
+    void* r = p;
+    int* s = (int*)r;
+    // !!! s = (int*)(int)1.5424243;
+    return 0;
 }
 
 
